4d42ac0237d1b5bf9c4cc92081ea0270ff58c724
Ahmed Nabil Awaad <ah.n.awaad@gmail.com>
2021-07-06 18:25:02 +0200

    Testing DAO

--- src/main/java/com/iti/wuzzufeda/services/Testing.java
+++ src/main/java/com/iti/wuzzufeda/services/Testing.java
@@ -1,6 +1,7 @@
 package com.iti.wuzzufeda.services;
 
 import com.iti.wuzzufeda.dao.JobsDAO;
+import com.iti.wuzzufeda.models.Job;
 import org.apache.log4j.Level;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
@@ -8,31 +9,24 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Testing {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         String filePath = "src/main/resources/Wuzzuf_Jobs.csv";
 
-//        Logger.getLogger("org").setLevel(Level.ERROR);
-//        Logger.getRootLogger().setLevel(Level.OFF);
-        List<Logger> loggers = Collections.<Logger>list(LogManager.getCurrentLoggers());
-        loggers.add(LogManager.getRootLogger());
-        for ( Logger logger : loggers ) {
-            logger.setLevel(Level.OFF);
-        }
+        List<Job> jobs = new ArrayList<>();
 
-        // Creating spark session
-        SparkSession sparkSession = SparkSession.builder().appName("Wuzzuf").master("local[*]").getOrCreate();
-
-        Dataset<Row> dataset = JobsDAO.readCSVUsingSpark(filePath, sparkSession);
-
-        dataset.printSchema();
-//        dataset.show();
+        File file = new File(filePath);
+        // Create list of lines
+        List<String> lines = Files.readAllLines(file.toPath());
+        System.out.println(lines);
 
     }
-
-
 }
