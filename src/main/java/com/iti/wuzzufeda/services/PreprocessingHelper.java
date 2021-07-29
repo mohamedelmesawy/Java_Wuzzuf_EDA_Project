eaa2c650d8bffd02cb72432ff24b8cb5ec1719e3
mohamedelmesawy1993 <mohamedelmesawy1993@gmail.com>
2021-07-30 01:02:22 +0200

    last feature

--- src/main/java/com/iti/wuzzufeda/services/PreprocessingHelper.java
+++ src/main/java/com/iti/wuzzufeda/services/PreprocessingHelper.java
@@ -1,10 +1,14 @@
 package com.iti.wuzzufeda.services;
 
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.ml.feature.OneHotEncoder;
 import org.apache.spark.ml.feature.StringIndexer;
 import org.apache.spark.ml.feature.StringIndexerModel;
+import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import scala.xml.MetaData;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -12,35 +16,30 @@ import java.util.stream.Collectors;
 public class PreprocessingHelper {
 
     public static Dataset<Row> removeNulls(Dataset<Row> dataset){
-        dataset = dataset.na().drop();
-        return dataset;
+        return dataset.na().drop();
     }
 
     public static Dataset<Row> removeDuplicates(Dataset<Row> dataset){
-        dataset = dataset.dropDuplicates();
-        return dataset;
+        return dataset.dropDuplicates();
     }
 
-    public static Dataset<Row> encodeCategoricalFeatures(Dataset<Row> dataset, List<String> features ){
-        Dataset<Row> newDataset = null;
-
+    public static Dataset<Row> encodeCategoricalFeatures(Dataset<Row> dataset, List<String> features){
         for (String feature: features){
             StringIndexerModel indexer = new StringIndexer()
                     .setInputCol(feature)
-                    .setOutputCol(feature + "_indexed")
+                    .setOutputCol(feature+"_indexed")
                     .fit(dataset);
-
-            newDataset = indexer.transform(dataset);
+            dataset = indexer.transform(dataset);
 
             OneHotEncoder encoder = new OneHotEncoder()
-                    .setInputCol(feature + "_indexed")
-                    .setOutputCol(feature + "_vec")
+                    .setInputCol(feature+"_indexed")
+                    .setOutputCol(feature+"_vec")
                     .setDropLast(true);
 
-            newDataset = encoder.transform(newDataset);
+            dataset = encoder.transform(dataset);
         }
-        return newDataset;
 
+        return dataset;
     }
 
     public static Map<String, Long> sortMap(Map<Row, Long> data){
@@ -71,6 +70,5 @@ public class PreprocessingHelper {
 
         return sortedData;
     }
-
 
 }
